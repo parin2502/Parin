f6d935be5c996420e073b0f8d81d631e7b625206
Parin <pvpandya1@myseneca.ca>
2022-09-14 15:01:07 -0400

    server.js updated

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Parin Pandya Student ID: 117878211 Date: 14/09/2002
+*
+*  Online (Cyclic) URL:  
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
